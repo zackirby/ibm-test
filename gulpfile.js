37f60d33467b5ed796fb8dccb9f3ff7586f143a0
Zack Kirby <zachk@netapp.com>
2017-02-05 01:41:25 -0500

    finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,7 @@
 var gulp = require('gulp'); // for gulp
 var sass = require('gulp-sass'); // for sass
 var browserSync = require('browser-sync').create(); // for live css reload
-var useref = require('gulp-useref'); // file concatenation
-var uglify = require('gulp-uglify'); // minify js
-var cssnano = require('gulp-cssnano'); // minify css
-var gulpIf = require('gulp-if'); // conditionally run a task
-var imagemin = require('gulp-imagemin'); // minify images
 var cache = require('gulp-cache'); // cache proxy
-var del = require('del'); // delete files/folders using globs - needed to delete 'dist' directory before build
 var runSequence = require('run-sequence'); // run series of dependent gulp tasks in order
 
 // convert sass to css
@@ -29,35 +23,6 @@ gulp.task('browserSync', function(){
 	});
 });
 
-// concatenate html partials; concatenate and minify css and js partials
-gulp.task('useref', function(){
-	return gulp.src('app/*.html')
-		.pipe(useref())
-		.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulpIf('*.css', cssnano()))
-		.pipe(gulp.dest('dist'))
-});
-
-// minify images
-gulp.task('images', function(){
-	return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-		.pipe(cache(imagemin({
-			interlaced: true
-		})))
-		.pipe(gulp.dest('dist/images'))
-});
-
-// move fonts to dist
-gulp.task('fonts', function(){
-	return gulp.src('app/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'))
-});
-
-//remove dist folder before build
-gulp.task('clean:dist', function(){
-	return del.sync('dist')
-});
-
 // watch for reload
 gulp.task('watch', function(){
 	gulp.watch('app/scss/**/*.scss', ['sass']);
@@ -65,14 +30,14 @@ gulp.task('watch', function(){
 });
 
 gulp.task('default', function(callback){
-	runSequence( ['browserSync', 'sass', 'useref', 'watch'], 
+	runSequence( ['browserSync', 'sass', 'watch'], 
 		callback
 	)
 });
 
 // build files
 gulp.task('build', function(callback){
-	runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'],
+	runSequence('sass',
 		callback
 	)
 });
